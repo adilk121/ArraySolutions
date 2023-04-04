94b1f706ef94facdc0d2930ed433614380a579a9
Saleem <128085215+saleem2819@users.noreply.github.com>
2023-04-04 20:01:15 +0530

    Update Merge_Two_Sorted_Lists.cpp

--- Merge_Two_Sorted_Lists.cpp
+++ Merge_Two_Sorted_Lists.cpp
@@ -1,12 +1,4 @@
-//Merge Two Sorted Lists
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
